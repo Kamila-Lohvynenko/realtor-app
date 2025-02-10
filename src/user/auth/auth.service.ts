8c8b70828d8939b92afd02cbdcb6e77ae06576dc
Kamila-Lohvynenko <kamila.lohvynenko@gmail.com>
2025-02-10 20:40:34 +0100

    hash password and create user

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,5 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable, ConflictException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
+import { UserType } from '@prisma/client';
 
 interface SignupParams {
   name: string;
@@ -12,7 +17,7 @@ interface SignupParams {
 export class AuthService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async signup({ email }: SignupParams) {
+  async signup({ email, password, name, phone }: SignupParams) {
     const userExists = await this.prismaService.user.findUnique({
       where: {
         email,
@@ -22,5 +27,19 @@ export class AuthService {
     if (userExists) {
       throw new ConflictException();
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.prismaService.user.create({
+      data: {
+        name,
+        email,
+        phone_number: phone,
+        password: hashedPassword,
+        user_type: UserType.BUYER,
+      },
+    });
+
+    return user;
   }
 }
