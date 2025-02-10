ec25513a24f7aeb77cd3a94249233b16731ad8e1
Kamila-Lohvynenko <kamila.lohvynenko@gmail.com>
2025-02-10 22:33:42 +0100

    implement sign in

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,14 +1,20 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { Injectable, ConflictException } from '@nestjs/common';
+import { Injectable, ConflictException, HttpException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
 import { UserType } from '@prisma/client';
 
 interface SignupParams {
   name: string;
   phone: string;
+  email: string;
+  password: string;
+}
+
+interface SigninParams {
   email: string;
   password: string;
 }
@@ -40,6 +46,43 @@ export class AuthService {
       },
     });
 
-    return user;
+    const token = this.generateJWT(name, user.id);
+
+    return token;
+  }
+
+  async signin({ email, password }: SigninParams) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new HttpException('Invalid credentials!', 400);
+    }
+
+    const hashedPassword = user.password;
+    const isValidPassword = await bcrypt.compare(password, hashedPassword);
+
+    if (!isValidPassword) {
+      throw new HttpException('Invalid credentials!', 400);
+    }
+    const token = this.generateJWT(user.name, user.id);
+
+    return token;
+  }
+
+  private generateJWT(name: string, id: number) {
+    const token = jwt.sign(
+      {
+        name,
+        id,
+      },
+      process.env.JSON_TOKEN_KEY as string,
+      { expiresIn: 3600000 },
+    );
+
+    return token;
   }
 }
